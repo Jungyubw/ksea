340da34c02920c5ddbebb39433481f17c9c6d8a7
Jungyubw <jungyubwnist@gmail.com>
2022-06-11 10:32:55 -0400

    fix

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-signin',
